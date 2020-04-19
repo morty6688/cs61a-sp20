10c8580d3e7a80375aff6b5e37baf294ce400e40
lijian12345 <1053294286@qq.com>
2020-04-19 21:47:03 +0800

    lab05 completed a half

---
+++ lab/lab05/lab05_extra.py
@@ -0,0 +1,143 @@
+""" Optional questions for Lab 05 """
+
+from lab05 import *
+
+
+def add_chars(w1, w2):
+    """
+    Return a string containing the characters you need to add to w1 to get w2.
+
+    You may assume that w1 is a subsequence of w2.
+
+    >>> add_chars("owl", "howl")
+    'h'
+    >>> add_chars("want", "wanton")
+    'on'
+    >>> add_chars("rat", "radiate")
+    'diae'
+    >>> add_chars("a", "prepare")
+    'prepre'
+    >>> add_chars("resin", "recursion")
+    'curo'
+    >>> add_chars("fin", "effusion")
+    'efuso'
+    >>> add_chars("coy", "cacophony")
+    'acphon'
+    >>> from construct_check import check
+    >>> # ban iteration and sets
+    >>> check(LAB_SOURCE_FILE, 'add_chars',
+    ...       ['For', 'While', 'Set', 'SetComp']) # Must use recursion
+    True
+    """
+    if not w1:
+        return w2
+    if w1[0] == w2[0]:
+        return add_chars(w1[1:], w2[1:])
+    else:
+        return w2[0] + add_chars(w1, w2[1:])
+
+
+def add_trees(t1, t2):
+    """
+    >>> numbers = tree(1,
+    ...                [tree(2,
+    ...                      [tree(3),
+    ...                       tree(4)]),
+    ...                 tree(5,
+    ...                      [tree(6,
+    ...                            [tree(7)]),
+    ...                       tree(8)])])
+    >>> print_tree(add_trees(numbers, numbers))
+    2
+      4
+        6
+        8
+      10
+        12
+          14
+        16
+    >>> print_tree(add_trees(tree(2), tree(3, [tree(4), tree(5)])))
+    5
+      4
+      5
+    >>> print_tree(add_trees(tree(2, [tree(3)]), tree(2, [tree(3), tree(4)])))
+    4
+      6
+      4
+    >>> print_tree(add_trees(tree(2, [tree(3, [tree(4), tree(5)])]), \
+    tree(2, [tree(3, [tree(4)]), tree(5)])))
+    4
+      6
+        8
+        5
+      5
+    """
+    "*** YOUR CODE HERE ***"
+
+
+# Shakespeare and Dictionaries
+def build_successors_table(tokens):
+    """Return a dictionary: keys are words; values are lists of successors.
+
+    >>> text = ['We', 'came', 'to', 'investigate', ',', 'catch', 'bad', 'guys', 'and', 'to', 'eat', 'pie', '.']
+    >>> table = build_successors_table(text)
+    >>> sorted(table)
+    [',', '.', 'We', 'and', 'bad', 'came', 'catch', 'eat', 'guys', 'investigate', 'pie', 'to']
+    >>> table['to']
+    ['investigate', 'eat']
+    >>> table['pie']
+    ['.']
+    >>> table['.']
+    ['We']
+    """
+    table = {}
+    prev = "."
+    for word in tokens:
+        if prev not in table:
+            "*** YOUR CODE HERE ***"
+        "*** YOUR CODE HERE ***"
+        prev = word
+    return table
+
+
+def construct_sent(word, table):
+    """Prints a random sentence starting with word, sampling from
+    table.
+
+    >>> table = {'Wow': ['!'], 'Sentences': ['are'], 'are': ['cool'], 'cool': ['.']}
+    >>> construct_sent('Wow', table)
+    'Wow!'
+    >>> construct_sent('Sentences', table)
+    'Sentences are cool.'
+    """
+    import random
+
+    result = ""
+    while word not in [".", "!", "?"]:
+        "*** YOUR CODE HERE ***"
+    return result.strip() + word
+
+
+def shakespeare_tokens(
+    path="shakespeare.txt", url="http://composingprograms.com/shakespeare.txt"
+):
+    """Return the words of Shakespeare's plays as a list."""
+    import os
+    from urllib.request import urlopen
+
+    if os.path.exists(path):
+        return open("shakespeare.txt", encoding="ascii").read().split()
+    else:
+        shakespeare = urlopen(url)
+        return shakespeare.read().decode(encoding="ascii").split()
+
+
+# Uncomment the following two lines
+# tokens = shakespeare_tokens()
+# table = build_successors_table(tokens)
+
+
+def random_sent():
+    import random
+
+    return construct_sent(random.choice(table["."]), table)
