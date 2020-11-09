7f741f9951a584fd483266c54658408229d81ee5
lijian12345 <1053294286@qq.com>
2020-11-10 00:27:39 +0800

    hw06 completed

--- hw/hw06/hw06.py
+++ hw/hw06/hw06.py
@@ -6,7 +6,7 @@ def survey(p):
     """
     You do not need to understand this code.
     >>> survey(passphrase)
-    'a874c00444b93ed81bc8dcde469ba5f4f3e7ddfcf994f7b8781821a1'
+    '9c557774afa3f7b5670f10a5ca54be0eedb8384a780375daa0340b45'
     """
     import hashlib
     return hashlib.sha224(p.encode('utf-8')).hexdigest()
--- hw/hw06/hw06.scm
+++ hw/hw06/hw06.scm
@@ -1,30 +1,30 @@
-;;;;;;;;;;;;;;;
-;; Questions ;;
-;;;;;;;;;;;;;;;
-
+; ;;;;;;;;;;;;;;
+; ; Questions ;;
+; ;;;;;;;;;;;;;;
 ; Scheme
+(define (cddr s) (cdr (cdr s)))
 
-(define (cddr s)
-  (cdr (cdr s)))
+(define (cadr s) (car (cdr s)))
 
-(define (cadr s)
-  'YOUR-CODE-HERE
-)
-
-(define (caddr s)
-  'YOUR-CODE-HERE
-)
+(define (caddr s) (car (cdr (cdr s))))
 
 (define (sign x)
-  'YOUR-CODE-HERE
-)
+  (cond 
+    ((< x 0) -1)
+    ((= x 0) 0)
+    (else    1)))
 
 (define (square x) (* x x))
 
 (define (pow b n)
-  'YOUR-CODE-HERE
-)
+  (cond 
+    ((= n 0)   1)
+    ((even? n) (square (pow b (/ n 2))))
+    (else      (* b (pow b (- n 1))))))
 
 (define (unique s)
-  'YOUR-CODE-HERE
-)
+  (if (null? s)
+      '()
+      (cons (car s)
+            (filter (lambda (x) (not (eq? x (car s))))
+                    (unique (cdr s))))))
